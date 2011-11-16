7a026a227bf7c0b89db1b0d2ceffc83c2cb4935e
danny <danny@danny-Laptop.home>
2011-11-15 19:08:41 -0500

    just doing some cleaning

--- src/com/commonsware/android/simon/GameScreen.java
+++ src/com/commonsware/android/simon/GameScreen.java
@@ -8,8 +8,8 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
 import android.view.View.OnClickListener;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -17,8 +17,6 @@ import android.widget.TextView;
 import com.commonsware.android.simon.SimonView.GameGUIThread;
 
 public class GameScreen extends Activity {
-	// private static final String LOG_TAG = GameScreen.class.getSimpleName();
-	private VortexView _vortexView;
 	
 	private EditText txtBoxUser;
 
--- src/com/commonsware/android/simon/SubmitScore.java
+++ src/com/commonsware/android/simon/SubmitScore.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 
+
 public class SubmitScore extends Activity {
 	GameDataBaseInterface db;
 	
